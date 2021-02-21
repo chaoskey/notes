4f5360e91d21fb1dc14c0fcfc52590d5100ba465
chaoskey <joistwang@sina.com>
2021-02-21 11:35:00 +0800

    发布

--- cn.search.min.ae40ea0b51c8339faa91e6c7185b6b7a562a2c37dc01ce6818eaab312cbe1d25.js
+++ cn.search.min.ae40ea0b51c8339faa91e6c7185b6b7a562a2c37dc01ce6818eaab312cbe1d25.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/cn.search-data.min.2bf753ff71e7866ceb3a437bfbfe3cebc88c8bc01d842ad0ab79e93c57627524.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/notes/flexsearch.min.js');loadScript('/notes/cn.search-data.min.3992fcc5abbb9ef53dab840c7cc56f0097fa805f24782adcee72949b61ba4116.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
